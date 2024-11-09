bca7a1a7e4940fabfa7d1aee4bb5cfbbb3cf85d8
chilinha <69139070+chilinha@users.noreply.github.com>
2024-11-09 09:43:03 +0800

    Update fofa.sh

--- speedtest/fofa.sh
+++ speedtest/fofa.sh
@@ -17,7 +17,7 @@ if [ $# -eq 0 ]; then
   echo "2. 北京联通"
   echo "3. 四川电信"
   echo "0. 全部"
-  read -t 10 -p "输入选择或在10秒内无输入将默认选择全部: " city_choice
+  read -t 1 -p "输入选择或在1秒内无输入将默认选择全部: " city_choice
 
   if [ -z "$city_choice" ]; then
       echo "未检测到输入，自动选择全部选项..."
@@ -110,8 +110,8 @@ while IFS= read -r line; do
     ip="$line"
     url="http://$ip/$stream"
     echo "$url"
-    curl "$url" --connect-timeout 3 --max-time 10 -o /dev/null >zubo.tmp 2>&1
-    a=$(head -n 5 zubo.tmp | awk '{print $NF}' | tail -n 1)
+    curl "$url" --connect-timeout 5 --max-time 15 -o /dev/null >zubo.tmp 2>&1
+    a=$(head -n 3 zubo.tmp | awk '{print $NF}' | tail -n 1)
 
     echo "第 $i/$lines 个：$ip $a"
     echo "$ip $a" >> "speedtest_${city}_$time.log"
@@ -123,8 +123,6 @@ cat "result/result_${city}.txt"
 ip1=$(awk 'NR==1{print $2}' result/result_${city}.txt)
 ip2=$(awk 'NR==2{print $2}' result/result_${city}.txt)
 ip3=$(awk 'NR==3{print $2}' result/result_${city}.txt)
-ip4=$(awk 'NR==4{print $2}' result/result_${city}.txt)
-ip5=$(awk 'NR==5{print $2}' result/result_${city}.txt)
 rm -f "speedtest_${city}_$time.log"
 
 # 用 3 个最快 ip 生成对应城市的 txt 文件
@@ -133,11 +131,9 @@ program="template/template_${city}.txt"
 sed "s/ipipip/$ip1/g" "$program" > tmp1.txt
 sed "s/ipipip/$ip2/g" "$program" > tmp2.txt
 sed "s/ipipip/$ip3/g" "$program" > tmp3.txt
-sed "s/ipipip/$ip4/g" "$program" > tmp4.txt
-sed "s/ipipip/$ip5/g" "$program" > tmp5.txt
-cat tmp1.txt tmp2.txt tmp3.txt tmp4.txt tmp5.txt > "txt/${city}.txt"
-
-rm -rf tmp1.txt tmp2.txt tmp3.txt tmp4.txt tmp5.txt
+cat tmp1.txt tmp2.txt tmp3.txt > "txt/${city}.txt"
+sed -i ‘/[/]{3}/d’ "txt/${city}.txt"
+rm -rf tmp1.txt tmp2.txt tmp3.txt
 
 
 #--------------------合并所有城市的txt文件为:   fofa.txt-----------------------------------------
